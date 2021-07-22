03d72d26f7207ddc64dcfe02373f91082dbe3566
Irvin Buendia <irvin.buendia1@gmail.com>
2021-07-22 17:05:16 -0500

    views to repo and jpa lecs

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,56 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 140)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    public Post(){}
+
+    public Post(Long id, String title, String body){
+        this.title = title;
+        this.body = body;
+        this.id = id;
+    }
+
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
