3bf2f41df8bbf5b74f8fc8002618b2a58a6ac05f
Irvin Buendia <irvin.buendia1@gmail.com>
2021-07-22 17:07:33 -0500

    pushing to views

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -6,6 +6,7 @@ import com.codeup.springblog.models.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
