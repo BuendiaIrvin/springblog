133637fdd79378bdc2f9abe70150ea060900d002
Irvin Buendia <irvin.buendia1@gmail.com>
2021-07-27 15:49:53 -0500

    mixed relatinship exc and services exc

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
+
+    private final EmailService emailSvc;
+
+    public HelloController(EmailService emailSvc){
+        this.emailSvc = emailSvc;
+    }
 
     @GetMapping("/hello")
     @ResponseBody
@@ -29,6 +36,7 @@ public class HelloController {
     @PostMapping("/join")
     public String joinCohort(@RequestParam(name= "cohort") String cohort, Model model){
         model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        emailSvc.prepareAndSend("test@test.com","hello! welcome to " + cohort +  "!", "thank you for attending our web dev program.");
         return "join";
     }
 
