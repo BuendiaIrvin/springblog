69fa58a116037b865811e2489d5d1d36fc964c42
Irvin Buendia <irvin.buendia1@gmail.com>
2021-07-29 14:30:04 -0500

    authentication lec

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -5,6 +5,7 @@ import com.codeup.springblog.models.PostRepository;
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.models.UserRepository;
 import com.codeup.springblog.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +17,13 @@ import java.util.List;
 public class PostController {
     private final PostRepository postDao;
     private final UserRepository userDao;
-    private final EmailService emailService;
 
-    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
-            this.postDao = postDao;
-            this.userDao = userDao;
-            this.emailService = emailService;
-        }
+    public PostController(PostRepository postDao, UserRepository userDao) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+    }
 
-
-        @GetMapping("/posts")
+    @GetMapping("/posts")
     public String viewPosts(Model model) {
         model.addAttribute("posts", postDao.findAll());
         return "posts/index";
@@ -33,28 +31,43 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
-        model.addAttribute("post", postDao.getById(id));
+        Post post = postDao.getById(id);
+        boolean isPostOwner = false;
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != "anonymousUser") {
+            User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            isPostOwner = currentUser.getId() == post.getUser().getId();
+        }
+        model.addAttribute("post", post);
+        model.addAttribute("isPostOwner", isPostOwner);
         return "posts/show";
     }
 
-    @GetMapping("/posts/edit/{id}")
+    @GetMapping("/posts/{id}/edit")
     public String editForm(@PathVariable long id, Model model) {
-        model.addAttribute("post", postDao.getById(id));
-        return "posts/edit";
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = postDao.getById(id);
+        if (currentUser.getId() == post.getUser().getId()) {
+            model.addAttribute("post", post);
+            return "posts/edit";
+        } else {
+            return "redirect:/posts/" + id;
+        }
     }
 
-    @PostMapping("/posts/edit/{id}")
-    public String editPost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
-        Post post = postDao.getById(id);
-        post.setTitle(title);
-        post.setBody(body);
+    @PostMapping("/posts/{id}/edit")
+    public String editPost(@PathVariable long id, @ModelAttribute Post post) {
+        post.setUser(userDao.getById(1L));
         postDao.save(post);
         return "redirect:/posts/" + id;
     }
 
     @PostMapping("/posts/delete/{id}")
     public String deletePost(@PathVariable long id) {
-        postDao.delete(postDao.getById(id));
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = postDao.getById(id);
+        if (currentUser.getId() == post.getUser().getId()) {
+            postDao.delete(post);
+        }
         return "redirect:/posts";
     }
 
@@ -62,7 +75,6 @@ public class PostController {
     public String showCreateForm(Model model) {
         model.addAttribute("post", new Post());
         return "posts/create";
-
     }
 
     @PostMapping("/posts/create")
@@ -70,9 +82,9 @@ public class PostController {
         User user = userDao.getById(1L);
         post.setUser(user);
         postDao.save(post);
-        emailService.prepareAndSend(post, "You created: " + post.getTitle(), post.getBody());
-
+//        emailService.prepareAndSend(post, "You created: " + post.getTitle(), post.getBody());
         return "redirect:/posts";
-
     }
+
+
 }
